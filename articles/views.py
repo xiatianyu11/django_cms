5fefc06e714fce942fc650ba02562fb4614b4eda
xiatianyu <xiatianyu@dbs.com>
2017-05-31 11:56:43 +0800

    update

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.forms.models import model_to_dict
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404
 from .models import Article
 from .forms import ArticleForm
@@ -14,21 +14,26 @@ def list(request):
 	return render(request, 'articles/index.html', {'latest_article_list' : latest_article_list})
 
 def add(request):
-	return render(request, 'articles/add.html')
+	if request.method == 'POST':
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			return render(request, 'success.html')
+		else:
+			return render(request, 'fail.html')
+	else:
+		form = ArticleForm()
+		return render(request, 'articles/edit.html', {'form' : form})
+
 
 def edit(request, article_id):
-	try:
-		article = Article.objects.get(pk=article_id)
-		data = model_to_dict(article)
-		form = ArticleForm(initial=data)
-		form.as_p()
-	except Article.DoesNotExist:
-		raise Http404("Article does not exist")
+	article = get_object_or_404(Article, pk=article_id)
+	form = ArticleForm(initial=model_to_dict(article))
 	return render(request, 'articles/edit.html', {'form' : form})
 
 def detail(request, article_id):
-	try:
-		article = Article.objects.get(pk=article_id)
-	except Article.DoesNotExist:
-		raise Http404("Article does not exist")
+#	try:
+#		article = Article.objects.get(pk=article_id)
+#	except Article.DoesNotExist:
+#		raise Http404("Article does not exist")
+	article = get_object_or_404(Article, pk=article_id)
 	return render(request, 'articles/detail.html', {'article' : article})
